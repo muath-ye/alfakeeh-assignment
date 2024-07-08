7c231c770540e33b02db85289b58666822e74945
muathye <34031333+muath-ye@users.noreply.github.com>
2024-07-08 14:27:33 +0300

    chore: update welcome screen with bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -3018,7 +3018,6 @@ function withinMaxClamp(min, value, max) {
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
-alert('Hello World');
 
 /***/ }),
 
